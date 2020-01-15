73a310cb79abe4ac520b02cb12152c7566468a53
AbderrazzakB <benbouya.abderrazzak@gmail.com>
2020-01-15 18:29:20 +0100

    feat: init the project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import i18n from './i18n'
+
+import "./assets/styles/index.scss";
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  i18n,
+  render: h => h(App)
 }).$mount('#app')
