9a4be824de1dc1da9d23439906bb81359b22af7d
AbderrazzakB <benbouya.abderrazzak@gmail.com>
2020-01-21 17:13:58 +0100

    fix: add install function

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import OrInputDate from "@/components/InputDate/OrInputDate.vue";
+import OrPickerDate from "@/components//PickerDate/OrPickerDate.vue";
 
-export default {
-  OrInputDate
-};
+const components = [OrInputDate, OrPickerDate];
+
+function install(Vue: any) {
+  for (const component in components) {
+    if (components[component]) {
+      Vue.component(components[component].name, components[component]);
+    }
+  }
+}
+
+export default { install, ...components };
